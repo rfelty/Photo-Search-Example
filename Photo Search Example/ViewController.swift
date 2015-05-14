5cf6097ff95a2f8e64bb79c2f93452ff00be1b52
rfelty <rfelty@gmail.com>
2015-05-14 16:53:09 -0400

    Moved code to correct unresolved identifier.

--- Photo Search Example/ViewController.swift
+++ Photo Search Example/ViewController.swift
@@ -19,21 +19,21 @@ class ViewController: UIViewController {
             parameters: nil,
             success: { (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
                 println("JSON: " + responseObject.description)
+                if let dataArray = responseObject["data"] as? [AnyObject] {
+                    var urlArray:[String] = []                  //1
+                    for dataObject in dataArray {               //2
+                        if let imageURLString = dataObject.valueForKeyPath("images.standard_resolution.url") as? String { //3
+                            urlArray.append(imageURLString)     //4
+                        }
+                    }
+                    println(urlArray)                           //5
+                }
             },
             failure: { (operation: AFHTTPRequestOperation!,error: NSError!) in
                 println("Error: " + error.localizedDescription)
         })
         // Do any additional setup after loading the view, typically from a nib.
         
-        if let dataArray = responseObject["data"] as? [AnyObject] {
-            var urlArray:[String] = []                  //1
-            for dataObject in dataArray {               //2
-                if let imageURLString = dataObject.valueForKeyPath("images.standard_resolution.url") as? String { //3
-                    urlArray.append(imageURLString)     //4
-                }
-            }
-            println(urlArray)                           //5
-        }
     }
 
     override func didReceiveMemoryWarning() {
