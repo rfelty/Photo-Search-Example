f49c6670df29aa01aee72162a52a43b7ce337997
rfelty <rfelty@gmail.com>
2015-05-13 23:22:58 -0400

    Set-up program correctly up to Lesson 4-1

--- Photo Search Example/ViewController.swift
+++ Photo Search Example/ViewController.swift
@@ -12,6 +12,16 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let manager = AFHTTPRequestOperationManager()
+        
+        manager.GET( "https://api.instagram.com/v1/tags/clararockmore/media/recent?client_id=3a20d60d663942e795b3e746b3d04c21",
+            parameters: nil,
+            success: { (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
+                println("JSON: " + responseObject.description)
+            },
+            failure: { (operation: AFHTTPRequestOperation!,error: NSError!) in
+                println("Error: " + error.localizedDescription)
+        })
         // Do any additional setup after loading the view, typically from a nib.
     }
 
---
+++ bridge.h
@@ -0,0 +1,13 @@
+//
+//  bridge.h
+//  Photo Search Example
+//
+//  Created by Rick Felty on 5/13/15.
+//  Copyright (c) 2015 Rick Felty. All rights reserved.
+//
+#import "AFNetworking.h"
+#ifndef Photo_Search_Example_bridge_h
+#define Photo_Search_Example_bridge_h
+
+
+#endif
