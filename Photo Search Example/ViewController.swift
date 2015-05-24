c04d72fd73bc18a8f6b0b01ea41b6329fcf7ac3b
rfelty <rfelty@gmail.com>
2015-05-24 18:26:44 -0400

    Final version with search enabled but no keyboard for some reason.

--- Photo Search Example/ViewController.swift
+++ Photo Search Example/ViewController.swift
@@ -8,47 +8,52 @@
 
 import UIKit
 
-
 class ViewController: UIViewController, UISearchBarDelegate {
+    
     @IBOutlet weak var scrollView: UIScrollView!
+    
+    //MARK: ViewController lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        let manager = AFHTTPRequestOperationManager()
+        searchInstagramByHashtag("dogs")
         
-        manager.GET( "https://api.instagram.com/v1/tags/clararockmore/media/recent?client_id=3a20d60d663942e795b3e746b3d04c21",
+    }
+    //MARK: Utility methods
+    func searchInstagramByHashtag(searchString:String) {
+        let manager = AFHTTPRequestOperationManager()
+        manager.GET( "https://api.instagram.com/v1/tags/\(searchString)/media/recent?client_id=3a20d60d663942e795b3e746b3d04c21",
             parameters: nil,
             success: { (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
-                println("JSON: " + responseObject.description)
                 if let dataArray = responseObject["data"] as? [AnyObject] {
-                    var urlArray:[String] = []                  //1
-                    for dataObject in dataArray {               //2
-                        if let imageURLString = dataObject.valueForKeyPath("images.standard_resolution.url") as? String { //3
-                            urlArray.append(imageURLString)     //4
+                    var urlArray:[String] = []
+                    for dataObject in dataArray {
+                        if let imageURLString = dataObject.valueForKeyPath("images.standard_resolution.url") as? String {
+                            urlArray.append(imageURLString)
                         }
                     }
-self.scrollView.contentSize = CGSizeMake(320, 320 * CGFloat(dataArray.count))
+                    //display urlArray in ScrollView
+                    self.scrollView.contentSize = CGSizeMake(320, 320 * CGFloat(dataArray.count))
+                    
                     for var i = 0; i < urlArray.count; i++ {
-                        let imageData = NSData(contentsOfURL: NSURL(string: urlArray[i])!)         //1
-                        if let imageDataUnwrapped = imageData {                                     //2
-                            let imageView = UIImageView(frame: CGRectMake(0, 320*CGFloat(i), 320, 320))     //1
-                            imageView.setImageWithURL( NSURL(string: urlArray[i]))                          //2
-                            self.scrollView.addSubview(imageView)                                       //5
-                        }
-                    }//5
+                        let imageView = UIImageView(frame: CGRectMake(0, 320*CGFloat(i), 320, 320))
+                        imageView.setImageWithURL( NSURL(string: urlArray[i]))
+                        self.scrollView.addSubview(imageView)
+                    }
+                    
                 }
             },
             failure: { (operation: AFHTTPRequestOperation!,error: NSError!) in
                 println("Error: " + error.localizedDescription)
         })
-        // Do any additional setup after loading the view, typically from a nib.
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK: UISearchBarDelegate protocol methods
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        for subview in self.scrollView.subviews {
+            subview.removeFromSuperview()
+        }
+        searchBar.resignFirstResponder()
+        searchInstagramByHashtag(searchBar.text)
+        
     }
-
-
 }
-
